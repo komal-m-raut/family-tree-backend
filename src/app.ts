2ebac21d8f306659f51a40e24c7e4d1f82e8a43e
Komal <komalraut3102@gmail.com>
2024-08-08 19:30:43 +0530

    server setup

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World! Hi there! how are you?");
+});
+
+export default app;
